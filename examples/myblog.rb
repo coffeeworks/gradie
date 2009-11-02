5998546de1f30a784e284435698d502708daed83
MoveGlobally Developer <mgdev@china.(none)>
2009-11-02 12:20:47 -0300

    Another example

--- examples/myblog.rb
+++ examples/myblog.rb
@@ -1,9 +1,17 @@
-require File.expand_path(File.dirname(__FILE__) + '/../lib/gradie')
+$:.unshift(File.dirname(__FILE__) + '/../lib/')
+require 'gradie'
 
-Gradie::Images.new(:dir => './examples/output') do
+Gradie::Images.new(:dir => './output') do
   image('background-header.png', :width => 5) do
     gradient :white, '#DADADA', :height => 50
     solid :red, :height => 10
     gradient :red, :black, :height => 50
+  end
+
+  image('stripes.png', :width => 10) do
+    10.times do
+      solid :black, :height => 10
+      solid :red, :height => 10
+    end
   end
 end
